35dad24c273d62a594a1c0435d15cb9a4341b7b6
sstefan23 <stefan.st223@gmail.com>
2025-03-06 17:22:09 +0000

    Fix Dive In lag - inline onclick like Get Mine!

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
 console.log("script.js loaded at: " + new Date().toLocaleTimeString());
 
-function scrollToProducts() {
-    console.log("Scroll triggered at: " + new Date().toLocaleTimeString());
-    document.getElementById('products').scrollIntoView({ behavior: 'smooth' });
-}
-
-// Attach listener directly
-const diveButton = document.querySelector('.cta-button');
-if (diveButton) {
-    diveButton.addEventListener('click', () => {
-        console.log("Click detected at: " + new Date().toLocaleTimeString());
-        scrollToProducts();
-    });
-} else {
-    console.error("Dive In button not found!");
-}
-
 const peekButtons = document.querySelectorAll('.peek-button');
 peekButtons.forEach(button => {
     button.addEventListener('click', () => {
