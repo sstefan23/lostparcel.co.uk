a09e7b069837d8f35f1c4dda6b95048162b18365
sstefan23 <stefan.st223@gmail.com>
2025-03-07 16:22:53 +0000

    Added more click debug logs and cursor fix

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ console.log("script.js loaded at: " + new Date().toLocaleTimeString());
 const diveInButton = document.querySelector('.hero .cta-button');
 if (diveInButton) {
     console.log("Button found!");
-    diveInButton.addEventListener('click', () => {
+    diveInButton.addEventListener('click', (e) => {
         console.log("Dive In clicked!");
         alert("Button works!");
     });
-    // Test clickability
     console.log("Is button clickable? " + (diveInButton.style.pointerEvents !== 'none'));
     diveInButton.addEventListener('mousedown', () => console.log("Mouse down on button"));
     diveInButton.addEventListener('touchstart', () => console.log("Touch start on button"));
+    // Force cursor and test
+    diveInButton.style.cursor = 'pointer';
+    diveInButton.addEventListener('click', (e) => console.log("Click event target: " + e.target.tagName));
 } else {
     console.log("Button NOT found!");
 }
