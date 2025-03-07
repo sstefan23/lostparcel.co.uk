a890f7005694153b2e134da2664648832e7e6bde
sstefan23 <stefan.st223@gmail.com>
2025-03-07 13:16:23 +0000

    Ultra-minimal script.js to test button and CSP

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 console.log("script.js loaded at: " + new Date().toLocaleTimeString());
 
 const diveInButton = document.querySelector('.hero .cta-button');
-diveInButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    alert("Dive In clicked!");
-    document.getElementById('products').scrollIntoView({ behavior: 'smooth' });
+diveInButton.addEventListener('click', () => {
+    console.log("Dive In clicked!");
+    alert("Button works!");
 });
