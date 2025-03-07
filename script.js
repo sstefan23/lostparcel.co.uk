ffd1faa417a175633a4dbb03077e78ac8748f1ee
sstefan23 <stefan.st223@gmail.com>
2025-03-07 14:49:57 +0000

    script change

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 console.log("script.js loaded at: " + new Date().toLocaleTimeString());
 const diveInButton = document.querySelector('.hero .cta-button');
-diveInButton.addEventListener('click', () => {
-    console.log("Dive In clicked!");
-    alert("Button works!");
-});
+if (diveInButton) {
+    console.log("Button found!");
+    diveInButton.addEventListener('click', () => {
+        console.log("Dive In clicked!");
+        alert("Button works!");
+    });
+} else {
+    console.log("Button NOT found!");
+}
