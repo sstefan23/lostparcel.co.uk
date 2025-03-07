df7742231224e216f6800b1a0a54d74b103e9e4e
sstefan23 <stefan.st223@gmail.com>
2025-03-07 13:33:03 +0000

    Removed _headers for clean CSP test

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 console.log("script.js loaded at: " + new Date().toLocaleTimeString());
-
 const diveInButton = document.querySelector('.hero .cta-button');
 diveInButton.addEventListener('click', () => {
     console.log("Dive In clicked!");
